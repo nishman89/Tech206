53f9c5a238ced0ae5984ff6ad885c3deba855800
nishman89 <53156243+nishman89@users.noreply.github.com>
2023-03-14 09:47:21 +0000

    Unit Test lesson refactored

---
+++ UnitTestLesson/CodeToTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeToTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOfDay = 21;
+            Greeting(timeOfDay);
+
+        }
+
+        private static void Greeting(int timeOfDay)
+        {
+            if (timeOfDay >= 5 && timeOfDay <= 12)
+            {
+                Console.WriteLine("Good morning!");
+            }
+            else if (timeOfDay >= 12 && timeOfDay <= 18)
+            {
+                Console.WriteLine("Good afternoon!");
+            }
+            else
+            {
+                Console.WriteLine("Good evening!");
+            }
+        }
+    }
+}
